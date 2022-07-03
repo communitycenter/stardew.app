10de919fd42ae1ad433d9ee9dc3612e56d936134
clxmente <solorioeclem164@gmail.com>
2022-07-03 12:24:23 -0700

    brought back the research folder which conflicted with next app creation

---
+++ research/processors/bundle_processor.py
@@ -0,0 +1,92 @@
+import json, re
+from operator import indexOf
+
+with open("../data/Bundles.json", "r") as f:
+    ObjInfo = json.load(f)
+
+with open("./items.json", "r") as f:
+    itemMapping = json.load(f)
+
+
+def spaceSplitter(s, spl, ind):
+    indx = [i for i, j in enumerate(s) if j == spl][ind - 1]
+    return [s[:indx], s[indx + 1 :]]
+
+
+def processTypeOfObject(bundleRewardType, bundleRewardItemID):
+    if bundleRewardType == "R":
+        return itemMapping["O"][bundleRewardItemID]
+    else:
+        return itemMapping[bundleRewardType][bundleRewardItemID]
+
+
+def processQualityOfItem(itemQuality):
+    if itemQuality == "0":
+        return "Common"
+    elif itemQuality == "1":
+        return "Silver"
+    elif itemQuality == "2":
+        return "Gold"
+    elif itemQuality == "3":
+        return "Iridium"
+
+
+bundles = {}
+for key, value in ObjInfo["content"].items():
+    bundleLocation = key.split("/")[0]
+    bundles[bundleLocation] = {}
+
+for key, value in ObjInfo["content"].items():
+    bundleLocation = key.split("/")[0]
+    spriteIndex = key.split("/")[1]
+
+    bundleName = value.split("/")[0]
+    bundleRewardType = value.split("/")[1].split(" ")[0]
+    bundleRewardItemID = value.split("/")[1].split(" ")[1]
+    bundleRewardItemQuantity = value.split("/")[1].split(" ")[2]
+
+    bundles[bundleLocation][bundleName] = {}
+
+    bundles[bundleLocation][bundleName]["items"] = {}
+    bundleRequiredItemArray = [
+        x for x in re.split(r"(.*?\s.*?\s.*?)\s", value.split("/")[2]) if x
+    ]
+    try:
+        bundleRequiredItemCount = value.split("/")[4]
+    except:
+        bundleRequiredItemCount = len(bundleRequiredItemArray)
+    bundles[bundleLocation][bundleName]["items"]["count"] = bundleRequiredItemCount
+
+    for item in bundleRequiredItemArray:
+        itemID = item.split(" ")[0]
+        itemQuantity = item.split(" ")[1]
+        itemQuality = item.split(" ")[2]
+
+        try:
+            itemName = itemMapping["O"][itemID]
+
+            bundles[bundleLocation][bundleName]["items"][itemName] = {}
+            bundles[bundleLocation][bundleName]["items"][itemName]["itemID"] = itemID
+            bundles[bundleLocation][bundleName]["items"][itemName][
+                "itemQuantity"
+            ] = itemQuantity
+            bundles[bundleLocation][bundleName]["items"][itemName][
+                "itemQuality"
+            ] = processQualityOfItem(itemQuality)
+        except:
+            pass
+
+    bundles[bundleLocation][bundleName]["reward"] = {}
+    bundles[bundleLocation][bundleName]["reward"]["itemType"] = bundleRewardType
+
+    bundles[bundleLocation][bundleName]["reward"]["itemName"] = processTypeOfObject(
+        bundleRewardType, bundleRewardItemID
+    )
+    bundles[bundleLocation][bundleName]["reward"]["itemID"] = bundleRewardItemID
+    bundles[bundleLocation][bundleName]["reward"][
+        "itemQuantity"
+    ] = bundleRewardItemQuantity
+
+
+with open("bundles.json", "w") as f:
+    json.dump(bundles, f, indent=4)
