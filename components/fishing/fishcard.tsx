4297404e6917ea5a8e51b3347be4f0667c273e2d
clxmente <solorioeclem164@gmail.com>
2022-08-01 19:37:01 -0700

    oauth fix

--- components/fishing/fishcard.tsx
+++ components/fishing/fishcard.tsx
@@ -1,7 +1,13 @@
 import type { Fish } from "../../types";
 
 import { CheckCircleIcon } from "@heroicons/react/outline";
-import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { useKV } from "../../hooks/useKV";
 
 type Props = {
@@ -36,12 +42,12 @@ function useSingleAndDoubleClick(
   return () => setClick((prev) => prev + 1);
 }
 
-const FishCard = ({
-  fish,
-  setSelectedFish,
-  setShowFish,
-}: Props) => {
-  const [checked, setChecked] = useKV<boolean>('fish', fish.itemID.toString(), false)
+const FishCard = ({ fish, setSelectedFish, setShowFish }: Props) => {
+  const [checked, setChecked] = useKV<boolean>(
+    "fish",
+    fish.itemID.toString(),
+    false
+  );
   const className = "h-5 w-5 " + (checked ? "text-green-500" : "hidden");
 
   const oneClick = useCallback(() => {
@@ -56,7 +62,7 @@ const FishCard = ({
   const click = useSingleAndDoubleClick(oneClick, twoClick);
   return (
     <div
-      className="relative select-none flex items-center space-x-3 rounded-lg border border-solid border-gray-300 bg-white py-4 px-5 hover:cursor-pointer hover:border-gray-400 dark:border-[#2A2A2A] dark:bg-[#1F1F1F]"
+      className="relative flex select-none items-center space-x-3 rounded-lg border border-solid border-gray-300 bg-white py-4 px-5 hover:cursor-pointer hover:border-gray-400 dark:border-[#2A2A2A] dark:bg-[#1F1F1F]"
       onClick={click}
     >
       <div className="flex-shrink-0">
--- pages/api/oauth/callback.ts
+++ pages/api/oauth/callback.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getCookie, setCookie } from 'cookies-next'
-import crypto from 'crypto'
-import { PrismaClient } from '@prisma/client'
-import { createToken } from '../kv'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getCookie, setCookie } from "cookies-next";
+import crypto from "crypto";
+import { PrismaClient } from "@prisma/client";
+import { createToken } from "../kv";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-type Data = Record<string, any>
+type Data = Record<string, any>;
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,78 +15,98 @@ export default async function handler(
 ) {
   try {
     // get state from cookie to verify that this is the correct authentication request
-    const state = getCookie('oauth_state', { req })
+    const state = getCookie("oauth_state", { req });
     if (!state) {
-      res.status(400).end()
-      return
+      res.status(400).end();
+      return;
     }
-    
-    const uid = getCookie('uid', { req })
+
+    const uid = getCookie("uid", { req });
     if (!uid) {
-      res.status(400).end()
-      return
+      res.status(400).end();
+      return;
     }
 
-    const code = req.query.code as string
+    const code = req.query.code as string;
     if (!code) {
-      res.status(400).end()
-      return
+      res.status(400).end();
+      return;
     }
 
-    const discord = await fetch(`https://discord.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(process.env.DISCORD_REDIRECT ?? "")}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        client_id: process.env.DISCORD_ID ?? "",
-        client_secret: process.env.DISCORD_SECRET ?? "",
-        grant_type: 'authorization_code',
-        code: code ?? "",
-        redirect_uri: process.env.DISCORD_REDIRECT ?? "",
-      }),
-    })
+    const discord = await fetch(
+      `https://discord.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(
+        process.env.DISCORD_REDIRECT ?? ""
+      )}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          client_id: process.env.DISCORD_ID ?? "",
+          client_secret: process.env.DISCORD_SECRET ?? "",
+          grant_type: "authorization_code",
+          code: code ?? "",
+          redirect_uri: process.env.DISCORD_REDIRECT ?? "",
+        }),
+      }
+    );
 
     if (!discord.ok) {
-      res.status(400).end()
-      return
+      res.status(400).end();
+      return;
     }
 
-    const discordData = await discord.json()
+    const discordData = await discord.json();
 
     const discordUser = await fetch(`https://discord.com/api/users/@me`, {
       headers: {
         Authorization: `Bearer ${discordData.access_token}`,
       },
-    })
+    });
 
     if (!discordUser.ok) {
-      res.status(400).end()
-      return
+      res.status(400).end();
+      return;
     }
 
-    const discordUserData = await discordUser.json()
+    const discordUserData = await discordUser.json();
 
-    let user = await prisma.users.findFirst({ where: { id: uid as string } })
-    let cookieSecret = user?.cookie_secret ?? crypto.randomBytes(16).toString('hex')
+    let user = await prisma.users.findFirst({ where: { id: uid as string } });
+    let cookieSecret =
+      user?.cookie_secret ?? crypto.randomBytes(16).toString("hex");
     if (!user) {
-      user = await prisma.users.create({
-        data: {
-          id: uid as string,
-          discord_id: discordUserData.id,
-          discord_name: discordUserData.username,
-          discord_avatar: discordUserData.avatar,
-          cookie_secret: cookieSecret
-        },
-      })
+      let discordUser = await prisma.users.findFirst({
+        where: { discord_id: discordUserData.id },
+      });
+
+      if (discordUser) {
+        user = discordUser;
+        cookieSecret = user.cookie_secret;
+      } else {
+        user = await prisma.users.create({
+          data: {
+            id: uid as string,
+            discord_id: discordUserData.id,
+            discord_name: discordUserData.username,
+            discord_avatar: discordUserData.avatar,
+            cookie_secret: cookieSecret,
+          },
+        });
+      }
     }
 
-    setCookie('uid', user.id, { req, res, domain: 'localhost', maxAge: 3600 })
-    const token = createToken(user.id, cookieSecret, 60 * 60 * 24 * 365)
-    setCookie('token', token.token, { req, res, domain: 'localhost', expires: new Date(token.expires * 1000) })
-    
-    res.redirect('/bundles')
+    setCookie("uid", user.id, { req, res, domain: "localhost", maxAge: 3600 });
+    const token = createToken(user.id, cookieSecret, 60 * 60 * 24 * 365);
+    setCookie("token", token.token, {
+      req,
+      res,
+      domain: "localhost",
+      expires: new Date(token.expires * 1000),
+    });
+
+    res.redirect("/fishing");
   } catch (e: any) {
-    res.status(500).send(e.message)
+    res.status(500).send(e.message);
   }
 }
