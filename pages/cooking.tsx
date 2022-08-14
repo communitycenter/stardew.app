df009ca686c36f7d794c8e7a5c7a31458c30f011
clxmente <solorioeclem164@gmail.com>
2022-08-14 00:41:55 -0700

    oops

--- pages/cooking.tsx
+++ pages/cooking.tsx
@@ -52,7 +52,7 @@ const Cooking: NextPage = () => {
       >
         <div className="mx-auto flex max-w-screen-2xl flex-shrink-0 items-center justify-between px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">
-            Fishing
+            Cooking
           </h1>
           <div>
             <label className="flex cursor-pointer flex-col items-center rounded-md border border-gray-300 bg-white p-1 text-white hover:border-gray-400 dark:border-[#2A2A2A] dark:bg-[#1F1F1F]">
