505f898c79d3ce88c9f544c65e2ff64e444abfb8
Jack LaFond <hi@jack.link>
2022-08-21 12:58:59 -0400

    fix(popup): button -> div

--- components/popup.tsx
+++ components/popup.tsx
@@ -58,14 +58,14 @@ export default function Popup({ user }: Props) {
                 </div>
                 <div className="py-1 font-normal">
                   <div>
-                    <button
+                    <div
                       onClick={() => setShowCreditsSlideover(true)}
                       className={classNames(
                         "group flex items-center rounded-md py-2 px-5 text-base font-normal text-[#7D7D7D] hover:cursor-pointer hover:bg-gray-50 hover:text-white  dark:border-[#2A2A2A] dark:bg-[#1F1F1F] dark:hover:bg-[#141414]"
                       )}
                     >
                       Credits
-                    </button>
+                    </div>
                   </div>
                   <div>
                     <a
