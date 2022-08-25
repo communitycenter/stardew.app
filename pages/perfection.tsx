b2b518a7a19650298343609456f24fa424a9d84b
Jack LaFond <hi@jack.link>
2022-08-25 00:53:16 -0400

    feat(perfection): add mining/monsters to page

--- pages/perfection.tsx
+++ pages/perfection.tsx
@@ -5,6 +5,8 @@ import SidebarLayout from "../components/sidebarlayout";
 import { useState } from "react";
 import Head from "next/head";
 
+import achievements from "../research/processors/data/achievements.json";
+
 import {
   ArchiveIcon,
   FilterIcon,
@@ -52,15 +54,26 @@ const Perfection: NextPage = () => {
       >
         <div className="mx-auto flex max-w-screen-2xl flex-shrink-0 items-center justify-between px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">
-            Perfection Tracking
+            Perfection
           </h1>
+          <div>
+            <label className="flex cursor-pointer flex-col items-center rounded-md border border-gray-300 bg-white p-1 text-white hover:border-gray-400 dark:border-[#2A2A2A] dark:bg-[#1F1F1F]">
+              <span className="flex justify-between">
+                {" "}
+                <FilterIcon
+                  className="h-5 w-5 text-black dark:bg-[#1F1F1F] dark:text-white"
+                  aria-hidden="true"
+                />
+              </span>
+            </label>
+          </div>
         </div>
         <div className="mx-auto max-w-screen-2xl px-4 sm:px-6 md:px-8">
-          <div className="grid grid-cols-1 gap-4 py-4">
-            <div className="mt-4 ml-1 text-2xl font-semibold text-gray-900 dark:text-white md:text-xl">
-              Mining
-            </div>
-            <div className="grid grid-cols-2 gap-4">
+          <div>
+            <h2 className="my-2 text-lg font-semibold text-gray-900 dark:text-white">
+              Mining & Monsters
+            </h2>
+            <div className="mt-2 grid grid-cols-2 gap-4">
               <InfoCard
                 title="Deepest Mining Level"
                 description={deepestMiningLevel.toString()}
@@ -72,20 +85,69 @@ const Perfection: NextPage = () => {
                 Icon={GiMining}
               />
             </div>
-            <div className="mt-4 ml-1 text-2xl font-semibold text-gray-900 dark:text-white md:text-xl">
-              Monsters
+
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <h2 className="mt-2 text-sm font-semibold text-gray-900 dark:text-white">
+                  Mining
+                </h2>
+                <div className="mt-2">
+                  {Object.values(achievements)
+                    .filter((achievement) => achievement.name === "The Bottom")
+                    .map((achievement) => (
+                      <AchievementCard
+                        id={achievement.id}
+                        tag={"achievements"}
+                        key={achievement.id}
+                        title={achievement.name}
+                        description={achievement.description}
+                        sourceURL={achievement.iconURL}
+                        initialChecked={deepestMiningLevel === 120}
+                        additionalDescription={`- ${
+                          120 - deepestMiningLevel
+                        } levels left!`}
+                      />
+                    ))}
+                </div>
+              </div>
+              <div>
+                <h2 className="mt-2 text-sm font-semibold text-gray-900 dark:text-white">
+                  Monsters
+                </h2>
+                <div className="mt-2">
+                  {Object.values(achievements)
+                    .filter(
+                      (achievement) => achievement.category === "monsters"
+                    )
+                    .map((achievement) => (
+                      <AchievementCard
+                        id={achievement.id}
+                        tag={"achievements"}
+                        key={achievement.id}
+                        title={achievement.name}
+                        description={achievement.description}
+                        sourceURL={achievement.iconURL}
+                        // initialChecked={deepestMiningLevel === 120}
+                        // additionalDescription={`- ${
+                        //   120 - deepestMiningLevel
+                        // } levels left!`}
+                      />
+                    ))}
+                </div>
+              </div>
             </div>
-            <div className="grid grid-cols-5 gap-4">
-              {Object.entries(monsters).map(([monster, monsterInfo]) => (
-                <MonsterCard
-                  key={monster}
-                  monsterInfo={monsterInfo}
-                  monsterCategory={monster}
-                  setSelectedMonster={setSelectedMonster}
-                  setShowMonster={setShowMonster}
-                />
-              ))}
-            </div>
+          </div>
+
+          <div className="mt-4 grid grid-cols-5 gap-4">
+            {Object.entries(monsters).map(([monster, monsterInfo]) => (
+              <MonsterCard
+                key={monster}
+                monsterInfo={monsterInfo}
+                monsterCategory={monster}
+                setSelectedMonster={setSelectedMonster}
+                setShowMonster={setShowMonster}
+              />
+            ))}
           </div>
         </div>
       </SidebarLayout>
