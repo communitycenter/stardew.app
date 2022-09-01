2dc89a0ec3ba4ec415fa70525bd83afe258a02c9
Jack LaFond <hi@jack.link>
2022-09-01 13:49:27 -0400

    feat(upload): hover cursor

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,108 +115,23 @@ const Home: NextPage = () => {
         sidebarOpen={sidebarOpen}
         setSidebarOpen={setSidebarOpen}
       >
-        {/* <div className="mx-auto flex h-screen max-w-2xl px-4 sm:px-0">
-          <div className="m-auto">
-            <div className="flex justify-center">
-              <Image
-                src={logo}
-                height={64}
-                width={64}
-                alt="Heart emoji"
-              ></Image>
-            </div>
-            <div className="justify-center space-y-2 text-center text-gray-900 dark:text-white">
-              <div className="text-2xl font-semibold">
-                Welcome to Stardew.app!{" "}
-                <Image
-                  src="https://stardewvalleywiki.com/mediawiki/images/c/c8/Emojis043.png"
-                  height={18}
-                  width={18}
-                  alt="Heart emoji"
-                ></Image>
-              </div>
-              <div className="pb-2 text-left">
-                <h2>
-                  Get started by heading to the sidebar and checking off what
-                  you&apos;ve done - or upload your save file below and
-                  automatically import your data. You can also find an upload
-                  button at the bottom of the sidebar on the left.
-                </h2>
-                <h2 className="pt-2">
-                  Additionally, you can login with Discord to save your data
-                  across devices.
-                </h2>
-              </div>
-              <div
-                className={classNames("dragAndDropWrapper", {
-                  dragAndDropActive: isDropActive,
-                })}
-              >
-                <DragAndDrop
-                  onDragStateChange={onDragStateChange}
-                  onFilesDrop={onFilesDrop}
-                >
-                  <label className="group flex cursor-pointer flex-col items-center rounded-lg border-2 border-dotted border-gray-300 bg-transparent p-10 text-white hover:border-gray-400">
-                    <PlusIcon
-                      className="h-8 w-8 text-gray-400 group-hover:text-gray-500"
-                      aria-hidden="true"
-                    />
-                    <p className="mt-4 text-sm text-gray-400 group-hover:text-gray-500">
-                      Upload Stardew Valley Save File
-                    </p>
-                    <input
-                      type="file"
-                      className="hidden"
-                      onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                        handleChange(e)
-                      }
-                    />
-                  </label>
-                </DragAndDrop>
-              </div>
-            </div>
-            <div className="mt-4 flex items-center justify-center gap-x-2">
-              {!user && (
-                <a
-                  className="
-                  relative flex items-center space-x-3 rounded-lg border border-solid border-gray-300 bg-white py-4 px-5 hover:cursor-pointer hover:border-gray-400 dark:border-[#2A2A2A] dark:bg-[#1F1F1F] dark:text-white"
-                >
-                  <Link key="Discord" href="/api/oauth">
-                    <span className="flex">
-                      <FaDiscord
-                        className="mr-3 h-5 w-5 flex-shrink-0 text-black dark:text-white"
-                        aria-hidden="true"
-                      />
-                      Login to Discord
-                    </span>
-                  </Link>
-                </a>
-              )}
-            </div>
-          </div>
-        </div> */}
-        {/* <div className="px-8">
-          <div className="space-y-4 ">
-            <div>
-              <div className="h-screenflex items-center space-x-3 truncate rounded-lg border border-gray-300 bg-[#f0f0f0] py-4 px-5 dark:border-[#2A2A2A] dark:bg-[#191919]">
-                welcome
-              </div>
-            </div>
-            <div className="flex flex-wrap space-x-4 md:flex-nowrap">
-              <div className="w-screen items-center space-x-3 truncate rounded-lg border border-gray-300 bg-[#f0f0f0] py-4 px-5 dark:border-[#2A2A2A] dark:bg-[#191919] ">
-                how to use site
-              </div>
-              <div className="w-screen items-center space-x-3 truncate rounded-lg border border-gray-300 bg-[#f0f0f0] py-4 px-5 dark:border-[#2A2A2A] dark:bg-[#191919]">
-                social links
-              </div>
-            </div>
-          </div>
-        </div> */}
         <div className="px-8">
           <div className="grid gap-4">
             <div>
               <div className=" h-[33vh] items-center space-x-3 truncate rounded-lg border border-gray-300 bg-[#f0f0f0] py-4 px-5 dark:border-[#2A2A2A] dark:bg-[#191919]">
-                welcome
+                <div>
+                  <div className="flex justify-center space-y-3">
+                    <div>
+                      <Image
+                        src={logo}
+                        width={64}
+                        height={64}
+                        alt="stardew.app"
+                      />
+                      <div>123</div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
@@ -233,12 +148,12 @@ const Home: NextPage = () => {
                 onFilesDrop={onFilesDrop}
               >
                 <div className="h-[25vh] items-center space-x-3 truncate rounded-lg border border-gray-300 bg-[#f0f0f0] hover:shadow-[inset_0_0px_10px] hover:shadow-green-300 hover:transition-shadow hover:duration-300 hover:ease-in-out dark:border-[#2A2A2A] dark:bg-[#191919]">
-                  <label className="flex h-full w-full flex-grow items-center justify-center">
+                  <label className="flex h-full w-full flex-grow items-center justify-center space-x-3 hover:cursor-pointer   ">
                     <PlusIcon
                       className="h-8 w-8 text-gray-400 group-hover:text-gray-500"
                       aria-hidden="true"
                     />
-                    <p className="mt-4 text-sm text-gray-400 group-hover:text-gray-500">
+                    <p className="text-sm text-gray-400 group-hover:text-gray-500">
                       Upload Stardew Valley Save File
                     </p>
                     <input
