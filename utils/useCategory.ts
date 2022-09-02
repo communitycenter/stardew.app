b02f9e33b10b5fc4f036e748a8d143c7448da0e3
Clemente Solorio <solorioeclem164@gmail.com>
2022-09-02 16:55:15 -0700

    fix(useCategory): check environment and set baseURL based on that

--- utils/useCategory.ts
+++ utils/useCategory.ts
@@ -1,9 +1,14 @@
 import useSWR from "swr";
 
 export function useCategory(category: string, type: string) {
+  const baseUrl = process.env.NEXT_PUBLIC_DEVELOPMENT
+    ? "http://localhost:3000"
+    : "https://stardew.app";
+
   const { data, error } = useSWR(
-    `http://localhost:3000/api/getCategoryInfo?category=${category}&type=${type}`,
-    (...opts) => fetch(...opts).then((r) => r.json())
+    `${baseUrl}/api/getCategoryInfo?category=${category}&type=${type}`,
+    //@ts-ignore
+    (...opts: any) => fetch(...opts).then((r) => r.json())
   );
 
   return {
