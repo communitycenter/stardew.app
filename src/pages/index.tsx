a3d9176b67752e402d40b210023c58ca04db0fde
Clemente Solorio <solorioeclem164@gmail.com>
2023-07-06 02:27:38 -0700

    haha oops it was wrong import for separator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
+
 import { presets } from "@/data/presets";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { PresetSelector } from "@/components/preset-selector";
-import { Separator } from "@radix-ui/react-separator";
 
 const inter = Inter({ subsets: ["latin"] });
-// i cant get the separator to work idunno
+
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
-      <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16 ">
+      <div className="flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16 px-7">
         <h2 className="text-lg font-semibold">stardew.app</h2>
         <div className="ml-auto flex w-full space-x-2 sm:justify-end">
           <PresetSelector presets={presets} />
@@ -18,7 +20,7 @@ export default function Home() {
           <Button>Log In With Discord</Button>
         </div>
       </div>
-      <Separator className="my-4" />
+      <Separator />
     </main>
   );
 }
