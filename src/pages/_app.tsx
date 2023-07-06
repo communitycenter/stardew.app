a6a471c0bf11d35d8887c015ab3760c3d5b4514c
Clemente Solorio <solorioeclem164@gmail.com>
2023-07-05 22:45:50 -0700

    init
    Co-authored-by: Jack LaFond <hi@jack.link>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
