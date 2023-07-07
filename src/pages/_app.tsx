dd16911b99f08045e49df8187c197d4333c9f087
Jack LaFond <jack.lafond@icloud.com>
2023-07-06 22:02:54 -0400

    fix overflow bug
    Co-authored-by: Nebula <nebula@railway.app>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <div className={`${inter.className}`}>
-        <Topbar />
+        <div className="sticky top-0 z-10 dark:bg-neutral-950">
+          <Topbar />
+        </div>
         <div className="">
           <Sidebar className="hidden md:flex md:fixed md:w-72 md:flex-col" />
           <div className="md:pl-72">
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="dark:bg-neutral-950">
+      <body className="dark:bg-neutral-950 overscroll-y-none">
         <Main />
         <NextScript />
       </body>
