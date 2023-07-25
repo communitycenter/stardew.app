c0cecad692bfa8843c00f0db042112bfbce8183f
Jack LaFond <jack.lafond@icloud.com>
2023-07-25 00:56:06 -0400

    feat: don't show farmer cards if no active player

--- src/pages/farmer.tsx
+++ src/pages/farmer.tsx
@@ -45,77 +45,79 @@ export default function Farmer() {
       <main
         className={`flex min-h-screen items-left md:border-l border-neutral-200 dark:border-neutral-800 ${inter.className} p-8`}
       >
-        <div className="mx-auto max-w-7xl w-full">
-          <div>
-            <div className="mb-2 mt-4 ml-1 text-2xl font-semibold text-gray-900 dark:text-white md:text-xl">
-              Farmer Information
+        <div className="mx-auto w-full">
+          {activePlayer && (
+            <div>
+              <div className="mb-3 -mt-[8.5px] ml-1 text-2xl font-semibold text-gray-900 dark:text-white md:text-xl">
+                Farmer Information
+              </div>
+              <div className="grid grid-cols-2 gap-4 xl:grid-cols-4">
+                <InfoCard
+                  title="Player Name"
+                  description={
+                    activePlayer
+                      ? activePlayer.general.name
+                      : "No Player Selected"
+                  }
+                  Icon={UserIcon}
+                />
+                <InfoCard
+                  title="Farm Information"
+                  description={
+                    activePlayer
+                      ? activePlayer.general.farmInfo
+                      : "No Player Selected"
+                  }
+                  Icon={HomeIcon}
+                />
+                <InfoCard
+                  title="Playtime"
+                  description={
+                    activePlayer
+                      ? activePlayer.general.timePlayed
+                      : "No Player Selected"
+                  }
+                  Icon={ClockIcon}
+                />
+                <InfoCard
+                  title="Money Earned"
+                  description={
+                    activePlayer
+                      ? `${activePlayer.general.totalMoneyEarned.toLocaleString()}g`
+                      : "No Player Selected"
+                  }
+                  Icon={CurrencyDollarIcon}
+                />
+                <InfoCard
+                  title="Farmer Level"
+                  description={
+                    activePlayer
+                      ? `${activePlayer.general.levels.Player}/25`
+                      : "No Player Selected"
+                  }
+                  Icon={ChartBarIcon}
+                />
+                <InfoCard
+                  title="Quests Completed"
+                  description={
+                    activePlayer
+                      ? activePlayer.general.questsCompleted.toString()
+                      : "No Player Selected"
+                  }
+                  Icon={BriefcaseIcon}
+                />
+                <InfoCard
+                  title="Stardrops Found"
+                  description={
+                    activePlayer
+                      ? activePlayer.general.stardropsCount.toString()
+                      : "No Player Selected"
+                  }
+                  Icon={StarIcon}
+                />
+              </div>
             </div>
-            <div className="grid grid-cols-2 gap-4 xl:grid-cols-4">
-              <InfoCard
-                title="Player Name"
-                description={
-                  activePlayer
-                    ? activePlayer.general.name
-                    : "No Player Selected"
-                }
-                Icon={UserIcon}
-              />
-              <InfoCard
-                title="Farm Information"
-                description={
-                  activePlayer
-                    ? activePlayer.general.farmInfo
-                    : "No Player Selected"
-                }
-                Icon={HomeIcon}
-              />
-              <InfoCard
-                title="Playtime"
-                description={
-                  activePlayer
-                    ? activePlayer.general.timePlayed
-                    : "No Player Selected"
-                }
-                Icon={ClockIcon}
-              />
-              <InfoCard
-                title="Money Earned"
-                description={
-                  activePlayer
-                    ? `${activePlayer.general.totalMoneyEarned.toLocaleString()}g`
-                    : "No Player Selected"
-                }
-                Icon={CurrencyDollarIcon}
-              />
-              <InfoCard
-                title="Farmer Level"
-                description={
-                  activePlayer
-                    ? `${activePlayer.general.levels.Player}/25`
-                    : "No Player Selected"
-                }
-                Icon={ChartBarIcon}
-              />
-              <InfoCard
-                title="Quests Completed"
-                description={
-                  activePlayer
-                    ? activePlayer.general.questsCompleted.toString()
-                    : "No Player Selected"
-                }
-                Icon={BriefcaseIcon}
-              />
-              <InfoCard
-                title="Stardrops Found"
-                description={
-                  activePlayer
-                    ? activePlayer.general.stardropsCount.toString()
-                    : "No Player Selected"
-                }
-                Icon={StarIcon}
-              />
-            </div>
-          </div>
+          )}
         </div>
       </main>
     </>
