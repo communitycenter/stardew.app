622371b435f010c899324ff279ecd4487dd549e2
Jack LaFond <jack.lafond@icloud.com>
2023-07-29 04:00:35 -0400

    fix: sorry about BigCraftable clem

--- src/components/cards/recipe-card.tsx
+++ src/components/cards/recipe-card.tsx
@@ -83,8 +83,8 @@ export const RecipeCard = <T extends Recipe>({
         src={iconURL}
         alt={name}
         className="rounded-sm"
-        width={16}
-        height={isCraftingRecipe(recipe) && recipe.isBigCraftable ? 32 : 16}
+        width={isCraftingRecipe(recipe) && recipe.isBigCraftable ? 16 : 32}
+        height={isCraftingRecipe(recipe) && recipe.isBigCraftable ? 16 : 32}
       />
       <div className="min-w-0 flex-1">
         <p className="font-medium truncate">{name}</p>
