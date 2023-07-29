0dc47d07a69a6281029dd40ee2ce74ad0bcce4f9
Jack LaFond <jack.lafond@icloud.com>
2023-07-29 04:41:41 -0400

    feat: museum parser

---
+++ src/lib/parsers/museum.ts
@@ -0,0 +1,70 @@
+import objects from "@/data/objects.json";
+
+export interface MuseumRet {
+  artifacts: number[];
+  minerals: number[];
+  artifactsDonated: number;
+  mineralsDonated: number;
+}
+
+export function parseMuseum(museumLocation: any): MuseumRet {
+  console.log(museumLocation);
+  /*
+    Achievements Relevant:
+      - Treasure Trove (donate 40 items to museum).
+      - A Complete Collection (donate every item to the museum).
+  */
+
+  let artifactsIds: number[] = [];
+  let mineralsIds: number[] = [];
+
+  let artifacts: number[] = [];
+  let minerals: number[] = [];
+
+  for (const key in objects) {
+    if (objects[key as keyof typeof objects].category === "Arch")
+      artifactsIds.push(parseInt(key));
+    if (objects[key as keyof typeof objects].category.startsWith("Minerals"))
+      mineralsIds.push(parseInt(key));
+  }
+
+  // now we check if any items have been donated
+  if (museumLocation.museumPieces === "")
+    return {
+      artifacts,
+      minerals,
+      artifactsDonated: 0,
+      mineralsDonated: 0,
+    };
+
+  // and if there are multiple types of items donated
+  if (typeof museumLocation.museumPieces.item.key === "undefined") {
+    // multiple items donated
+    for (const idx in museumLocation.museumPieces.item) {
+      let piece = museumLocation.museumPieces.item[idx];
+      let item_id = piece.value.int;
+
+      if (artifactsIds.includes(item_id)) artifacts.push(parseInt(item_id));
+      else if (mineralsIds.includes(item_id)) minerals.push(parseInt(item_id));
+    }
+  } else {
+    // only one item donated
+    let piece = museumLocation.museumPieces.item;
+    let item_id = piece.value.int;
+
+    if (artifactsIds.includes(item_id)) artifacts.push(parseInt(item_id));
+    else if (mineralsIds.includes(item_id)) minerals.push(parseInt(item_id));
+  }
+
+  const artifactsDonated = Object.values(artifacts).filter((v) => v).length;
+  const mineralsDonated = Object.values(minerals).filter((v) => v).length;
+
+  console.log(artifacts, minerals);
+
+  return {
+    artifacts,
+    minerals,
+    artifactsDonated,
+    mineralsDonated,
+  };
+}
