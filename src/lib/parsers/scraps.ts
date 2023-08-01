f8350666dea45caf12667ac616c97c6897a4d078
Jack LaFond <jack.lafond@icloud.com>
2023-08-01 15:25:02 -0400

    feat: scrap parser

---
+++ src/lib/parsers/scraps.ts
@@ -0,0 +1,32 @@
+import scraps from "@/data/journal_scraps.json";
+
+export interface ScrapsRet {
+  total: number;
+  found: { [key: string]: boolean };
+}
+
+export function parseScraps(player: any) {
+  const notesIDs = Object.keys(scraps);
+
+  let notesObject: { [key: string]: boolean } = {};
+
+  notesIDs.map((id) => (notesObject[id] = false));
+
+  if (!player.secretNotesSeen.int)
+    return {
+      total: 0,
+      found: notesObject,
+    };
+
+  const found = player.secretNotesSeen.int
+    .filter((scrap: number) => scrap >= 1000)
+    .map((note: number) => {
+      notesObject[note] = true;
+    });
+
+  return {
+    total: player.secretNotesSeen.int.filter((scrap: number) => scrap >= 1000)
+      .length,
+    found: notesObject,
+  };
+}
