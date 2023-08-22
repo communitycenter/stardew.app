188d71a43bbca61b555e961e997c6de82fb86001
Clemente Solorio <solorioeclem164@gmail.com>
2023-08-22 11:06:52 -0700

    feat: shipping manual update in new dialog

--- src/components/cards/shipping-card.tsx
+++ src/components/cards/shipping-card.tsx
@@ -5,31 +5,68 @@ import objects from "@/data/objects.json";
 import type { ShippingItem } from "@/types/items";
 
 import { cn } from "@/lib/utils";
+import { useContext, useMemo, useState } from "react";
 
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
+import { PlayersContext } from "@/contexts/players-context";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { IconChevronRight } from "@tabler/icons-react";
 
 interface Props {
   item: ShippingItem;
-  count: number;
-  status: 0 | 1 | 2;
 }
 
-export const ShippingCard = ({ item, count, status }: Props) => {
-  let colorClass = "";
-  switch (status) {
-    case 1:
-      colorClass =
-        "border-yellow-900 bg-yellow-500/20 hover:bg-yellow-500/30 dark:bg-yellow-500/10 hover:dark:bg-yellow-500/20";
-      break;
-    case 2:
-      colorClass =
-        "border-green-900 bg-green-500/20 hover:bg-green-500/30 dark:bg-green-500/10 hover:dark:bg-green-500/20";
-      break;
-    default:
-      colorClass =
-        "border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950 hover:bg-neutral-100 dark:hover:bg-neutral-800";
-      break;
-  }
+const classes = [
+  "border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950 hover:bg-neutral-100 dark:hover:bg-neutral-800",
+  "border-yellow-900 bg-yellow-500/20 hover:bg-yellow-500/30 dark:bg-yellow-500/10 hover:dark:bg-yellow-500/20",
+  "border-green-900 bg-green-500/20 hover:bg-green-500/30 dark:bg-green-500/10 hover:dark:bg-green-500/20",
+];
+
+export const ShippingCard = ({ item }: Props) => {
+  const { activePlayer, patchPlayer } = useContext(PlayersContext);
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(0);
+
+  const [_status, _count] = useMemo(() => {
+    if (
+      !activePlayer ||
+      !activePlayer.shipping ||
+      !activePlayer.shipping.shipped ||
+      !activePlayer.shipping.shipped[item.itemID]
+    )
+      return [0, 0];
+
+    // status = 0 => not shipped
+    // status = 1 => counts for polyculture but count < 15
+    // status = 2 => count > 0 if not polyculture, count >= 15 if polyculture
+    let _status = 0;
+    const _count = activePlayer.shipping.shipped[item.itemID];
+
+    if (_count === null) return [0, 0];
+
+    if (item.polyculture) {
+      if (_count >= 15) _status = 2;
+      else _status = 1;
+    } else {
+      if (_count > 0) _status = 2;
+    }
+
+    return [_status, _count];
+  }, [activePlayer, item]);
+
   const iconURL =
     objects[item.itemID.toString() as keyof typeof objects].iconURL;
 
@@ -38,30 +75,94 @@ export const ShippingCard = ({ item, count, status }: Props) => {
   const description =
     objects[item.itemID.toString() as keyof typeof objects].description;
 
+  async function handleSave() {
+    if (!activePlayer) return;
+
+    // don't make any requests if the value hasn't changed
+    if (value === _count || value < 0) {
+      setOpen(false);
+      return;
+    }
+
+    const patch = {
+      shipping: {
+        shipped: {
+          [item.itemID]: value === 0 ? null : value,
+        },
+      },
+    };
+
+    await patchPlayer(patch);
+    setOpen(false);
+  }
   return (
-    <a
-      className={cn(
-        "flex select-none items-center text-left space-x-3 rounded-lg border py-4 px-5 text-neutral-950 dark:text-neutral-50 shadow-sm hover:cursor-pointer",
-        colorClass
-      )}
-      target="_blank"
-      rel="noreferrer"
-      href={`https://stardewvalleywiki.com/${name.replaceAll(" ", "_")}`}
-    >
-      <Image
-        src={iconURL}
-        alt={name}
-        className="rounded-sm"
-        width={32}
-        height={32}
-      />
-      <div className="min-w-0 flex-1 pr-3">
-        <p className="font-medium truncate">{`${name} (${count}x)`}</p>
-        <p className="truncate text-sm text-neutral-500 dark:text-neutral-400">
-          {description}
-        </p>
-      </div>
-      <ArrowTopRightOnSquareIcon className="w-5 h-5 text-neutral-500 dark:text-neutral-400" />
-    </a>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <div
+          className={cn(
+            "flex select-none items-center text-left space-x-3 rounded-lg border py-4 px-5 text-neutral-950 dark:text-neutral-50 shadow-sm hover:cursor-pointer transition-colors",
+            classes[_status]
+          )}
+        >
+          <Image
+            src={iconURL}
+            alt={name}
+            className="rounded-sm"
+            width={32}
+            height={32}
+          />
+          <div className="min-w-0 flex-1 pr-3">
+            <p className="font-medium truncate">{`${name} (${_count}x)`}</p>
+            <p className="truncate text-sm text-neutral-500 dark:text-neutral-400">
+              {description}
+            </p>
+          </div>
+          <IconChevronRight className="w-5 h-5 text-neutral-500 dark:text-neutral-400" />
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <Image
+            src={iconURL}
+            alt={name}
+            className="rounded-sm mx-auto"
+            width={42}
+            height={42}
+          />
+          <DialogTitle className="text-center">{name}</DialogTitle>
+          <DialogDescription className="text-center">
+            {description}
+          </DialogDescription>
+        </DialogHeader>
+        <Label htmlFor="count">Amount Shipped:</Label>
+        <Input
+          id="count"
+          type="number"
+          min={0}
+          defaultValue={_count ?? 0}
+          onChange={(e) => setValue(parseInt(e.target.value))}
+        />
+        <DialogFooter className="sm:justify-between gap-3 sm:gap-0">
+          <Button variant="outline" asChild>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`https://stardewvalleywiki.com/${name.replaceAll(
+                " ",
+                "_"
+              )}`}
+            >
+              Visit Wiki Page
+            </a>
+          </Button>
+          <div className="flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 gap-3 sm:gap-0">
+            <Button variant="secondary" onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+            <Button onClick={() => handleSave()}>Save</Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
--- src/pages/shipping.tsx
+++ src/pages/shipping.tsx
@@ -4,7 +4,7 @@ import objects from "@/data/objects.json";
 import shipping_items from "@/data/shipping.json";
 import achievements from "@/data/achievements.json";
 
-import { useContext, useEffect, useMemo, useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import { PlayersContext } from "@/contexts/players-context";
 
 import {
@@ -25,17 +25,14 @@ const reqs: Record<string, number> = {
 };
 
 export default function Shipping() {
-  const [basicShipped, setBasicShipped] = useState({});
-
   const [search, setSearch] = useState("");
   const [_filter, setFilter] = useState("all");
 
   const { activePlayer } = useContext(PlayersContext);
 
-  useEffect(() => {
-    if (activePlayer && activePlayer.shipping?.shipped) {
-      setBasicShipped(activePlayer.shipping.shipped);
-    }
+  const basicShipped = useMemo(() => {
+    if (!activePlayer || !activePlayer.shipping?.shipped) return {};
+    return activePlayer.shipping.shipped;
   }, [activePlayer]);
 
   const [polycultureCount, monocultureAchieved, basicShippedCount] =
@@ -203,13 +200,14 @@ export default function Shipping() {
                     return !(i.itemID in basicShipped);
                   } else if (_filter === "1") {
                     // Polyculture crops that need completing
+                    // TODO: should we show all polyculture crops here?
                     return (
                       i.itemID in basicShipped &&
                       i.itemID in shipping_items &&
                       shipping_items[
                         i.itemID.toString() as keyof typeof shipping_items
                       ].polyculture &&
-                      basicShipped[i.itemID as keyof typeof basicShipped] <= 15
+                      basicShipped[i.itemID as keyof typeof basicShipped]! < 15
                     );
                   } else if (_filter === "2") {
                     // Shipped/Completed (we won't check for monoculture here)
@@ -217,33 +215,14 @@ export default function Shipping() {
                       shipping_items[
                         i.itemID.toString() as keyof typeof shipping_items
                       ].polyculture
-                      ? basicShipped[i.itemID as keyof typeof basicShipped] >=
+                      ? basicShipped[i.itemID as keyof typeof basicShipped]! >=
                           15
-                      : basicShipped[i.itemID as keyof typeof basicShipped] >=
+                      : basicShipped[i.itemID as keyof typeof basicShipped]! >=
                           1;
                   } else return true; // all recipes
                 })
                 .map((i) => (
-                  <ShippingCard
-                    key={i.itemID}
-                    item={i}
-                    count={
-                      basicShipped[i.itemID as keyof typeof basicShipped] ?? 0
-                    }
-                    status={
-                      i.itemID in basicShipped
-                        ? shipping_items[
-                            i.itemID.toString() as keyof typeof shipping_items
-                          ].polyculture
-                          ? basicShipped[
-                              i.itemID as keyof typeof basicShipped
-                            ] >= 15
-                            ? 2
-                            : 1
-                          : 2
-                        : 0
-                    }
-                  />
+                  <ShippingCard key={i.itemID} item={i} />
                 ))}
             </div>
           </section>
