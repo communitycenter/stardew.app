44ac165834a61dd6041b3955e8c399253e61d3ae
Jack LaFond <jack.lafond@icloud.com>
2023-08-26 15:04:32 -0400

    fix: oops

--- src/components/sheets/mobile-nav.tsx
+++ src/components/sheets/mobile-nav.tsx
@@ -96,10 +96,7 @@ export const MobileNav = ({
                 </Button>
               )}
               {!api.data?.discord_id && (
-                <Button
-                  className="dark:hover:bg-[#5865F2] hover:bg-[#5865F2] dark:hover:text-white"
-                  asChild
-                >
+                <Button className="dark:hover:bg-[#5865F2] hover:bg-[#5865F2] dark:hover:text-white">
                   {/* <Link href="/api/oauth">Log In With Discord</Link> */}
                   Log In With Discord
                 </Button>
