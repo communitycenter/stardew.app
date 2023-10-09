dc596a9c7f39004efd0eafff69b3dc8ebbccf892
Jack LaFond <jack.lafond@icloud.com>
2023-10-09 18:38:06 -0400

    partial: redesign fish sheet

--- src/components/sheets/fish-sheet.tsx
+++ src/components/sheets/fish-sheet.tsx
@@ -17,6 +17,7 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import clsx from "clsx";
 import * as Fathom from "fathom-client";
 import { CreatePlayerRedirect } from "../createPlayerRedirect";
 
@@ -25,6 +26,43 @@ interface Props {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
   fish: FishType | null;
 }
+
+type Seasons = "Winter" | "Spring" | "Summer" | "Fall";
+type Weather = "Sunny" | "Rainy" | "Both";
+
+const Seasons: Record<Seasons, { icon: string; color: string }> = {
+  Winter: {
+    icon: "https://stardewvalleywiki.com/mediawiki/images/thumb/6/60/Emojis103.png/48px-Emojis103.png",
+    color: "bg-blue-50 border border-blue-100",
+  },
+  Spring: {
+    icon: "https://stardewvalleywiki.com/mediawiki/images/thumb/d/d9/Emojis111.png/48px-Emojis111.png",
+    color: "bg-green-50 border border-green-100",
+  },
+  Summer: {
+    icon: "https://stardewvalleywiki.com/mediawiki/images/thumb/0/00/Emojis099.png/48px-Emojis099.png",
+    color: "bg-yellow-50 border border-yellow-100",
+  },
+  Fall: {
+    icon: "https://stardewvalleywiki.com/mediawiki/images/thumb/2/2b/Emojis110.png/48px-Emojis110.png",
+    color: "bg-orange-50 border border-orange-100",
+  },
+};
+
+const Weather: Record<Weather, { icon: string; color: string }> = {
+  Sunny: {
+    icon: "https://stardewvalleywiki.com/mediawiki/images/thumb/0/00/Emojis099.png/48px-Emojis099.png",
+    color: "bg-yellow-50 border border-yellow-100",
+  },
+  Rainy: {
+    icon: "https://stardewvalleywiki.com/mediawiki/images/thumb/2/2f/Emojis100.png/48px-Emojis100.png",
+    color: "bg-blue-50 border border-blue-100",
+  },
+  Both: {
+    icon: "https://stardewvalleywiki.com/mediawiki/images/thumb/1/1a/Emojis097.png/48px-Emojis097.png",
+    color: "bg-blue-50 border border-blue-100",
+  },
+};
 
 export const FishSheet = ({ open, setIsOpen, fish }: Props) => {
   const { activePlayer, patchPlayer } = useContext(PlayersContext);
@@ -117,33 +155,45 @@ export const FishSheet = ({ open, setIsOpen, fish }: Props) => {
             </section>
             <section className="space-y-2">
               <h3 className="font-semibold">Location</h3>
-              <Separator />
-              <ul className="list-disc list-inside">
+              {/* <Separator /> */}
+              <ul className="grid grid-cols-2 gap-2">
                 {fish.locations.map((location) => (
-                  <li
+                  <div
                     key={location}
-                    className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
+                    className="rounded-full p-2 flex gap-1 space-x-2 items-center text-sm text-neutral-500 dark:text-neutral-800 bg-neutral-100 dark:bg-neutral-800 border border-neutral-200 dark:border-neutral-700"
                   >
-                    {location}
-                  </li>
+                    <span className="ml-2">{location}</span>
+                  </div>
                 ))}
               </ul>
             </section>
             {!fish.trapFish && (
               <>
                 <section className="space-y-2">
-                  <h3 className="font-semibold">Season</h3>
-                  <Separator />
-                  <ul className="list-disc list-inside">
-                    {fish.seasons.map((season) => (
-                      <li
+                  <h3 className="font-semibold">Seasons</h3>
+                  {/* <Separator /> */}
+                  <div className="grid grid-cols-2 gap-2">
+                    {fish.seasons.map((season, index) => (
+                      <div
                         key={season}
-                        className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
+                        className={clsx(
+                          "rounded-full p-2 flex gap-1 space-x-2 items-center",
+                          Seasons[season as Seasons].color
+                        )}
                       >
-                        {season}
-                      </li>
+                        <Image
+                          src={Seasons[season as Seasons].icon}
+                          alt={""}
+                          width={20}
+                          height={20}
+                          className="ml-2"
+                        />
+                        <p className="text-sm text-neutral-500 dark:text-neutral-800">
+                          {season}
+                        </p>
+                      </div>
                     ))}
-                  </ul>
+                  </div>
                 </section>
                 <section className="space-y-2">
                   <h3 className="font-semibold">Time</h3>
@@ -155,9 +205,23 @@ export const FishSheet = ({ open, setIsOpen, fish }: Props) => {
                 <section className="space-y-2">
                   <h3 className="font-semibold">Weather</h3>
                   <Separator />
-                  <p className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm">
-                    {fish.weather}
-                  </p>
+                  <div
+                    className={clsx(
+                      "rounded-full p-2 flex gap-1 space-x-2 items-center",
+                      Weather[fish.weather as Weather].color
+                    )}
+                  >
+                    <Image
+                      src={Weather[fish.weather as Weather].icon}
+                      alt={""}
+                      width={20}
+                      height={20}
+                      className="ml-2"
+                    />
+                    <p className="text-sm text-neutral-500 dark:text-neutral-800">
+                      {fish.weather}
+                    </p>
+                  </div>
                 </section>
                 <section className="space-y-2">
                   <h3 className="font-semibold">Difficulty</h3>
