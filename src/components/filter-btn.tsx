348fadf5e82db011c1bf6fe1debb997b6e6b5cd5
Jack LaFond <jack.lafond@icloud.com>
2023-10-12 21:27:09 -0400

    fix(filter): hover state

--- src/components/filter-btn.tsx
+++ src/components/filter-btn.tsx
@@ -110,7 +110,7 @@ export const FilterSearch = ({
       <PopoverTrigger asChild>
         <div
           aria-expanded={open}
-          className="flex items-center border rounded-md space-x-3 px-3 text-sm outline-none text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:text-neutral-400 dark:border-neutral-800"
+          className="flex items-center border rounded-md space-x-3 px-3 text-sm outline-none text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:text-neutral-400 dark:border-neutral-800 hover:cursor-pointer"
         >
           <Icon className="h-4 w-4 shrink-0 opacity-50" />
           <p className="text-sm">
