a5b3078c7a55e116d430dd456ca4a764acdba52c
Clemente Solorio <solorioeclem164@gmail.com>
2023-11-14 12:03:44 -0800

    toast message in walnuts

--- src/lib/parsers/walnuts.ts
+++ src/lib/parsers/walnuts.ts
@@ -9,39 +9,49 @@ export const walnuts = allWalnuts as unknown as { [key: string]: WalnutType };
 
 export function parseWalnuts(save: any): WalnutRet {
   //   if (!player.mailReceived.string.includes("Visited_Island")) return;
+  try {
+    let collectedGoldenWalnuts: string[] = [];
 
-  let collectedGoldenWalnuts: string[] = [];
+    if (Array.isArray(save.collectedNutTracker.string)) {
+      collectedGoldenWalnuts = save.collectedNutTracker.string;
+    } else {
+      if (typeof save.collectedNutTracker.string !== "undefined") {
+        collectedGoldenWalnuts.push(save.collectedNutTracker.string);
+      }
+    }
 
-  if (Array.isArray(save.collectedNutTracker.string)) {
-    collectedGoldenWalnuts = save.collectedNutTracker.string;
-  } else {
-    collectedGoldenWalnuts.push(save.collectedNutTracker.string);
-  }
+    const internalWalnutIds = Object.keys(walnuts);
 
-  const internalWalnutIds = Object.keys(walnuts);
+    let trackedWalnutObject: { [key: string]: number } = {};
 
-  let trackedWalnutObject: { [key: string]: number } = {};
+    internalWalnutIds.map((id) => (trackedWalnutObject[id] = 0));
 
-  internalWalnutIds.map((id) => (trackedWalnutObject[id] = 0));
+    if (collectedGoldenWalnuts.length > 0) {
+      console.log(collectedGoldenWalnuts);
+      collectedGoldenWalnuts.forEach((nut) => {
+        trackedWalnutObject[nut] = walnuts[nut as keyof typeof walnuts].num;
+      });
+    }
 
-  collectedGoldenWalnuts.forEach((nut) => {
-    trackedWalnutObject[nut] = walnuts[nut as keyof typeof walnuts].num;
-  });
+    trackedWalnutObject["GoldenCoconut"] = save.goldenCoconutCracked
+      ? walnuts.GoldenCoconut.num
+      : 0;
 
-  trackedWalnutObject["GoldenCoconut"] = save.goldenCoconutCracked
-    ? walnuts.GoldenCoconut.num
-    : 0;
+    if (!save.limitedNutDrops?.item?.length)
+      return {
+        found: trackedWalnutObject,
+      };
 
-  if (!save.limitedNutDrops?.item?.length)
+    (save.limitedNutDrops?.item ?? []).forEach((k: any) => {
+      trackedWalnutObject[k.key.string as string] = k.value.int as number;
+    });
+
     return {
       found: trackedWalnutObject,
     };
-
-  (save.limitedNutDrops?.item ?? []).forEach((k: any) => {
-    trackedWalnutObject[k.key.string as string] = k.value.int as number;
-  });
-
-  return {
-    found: trackedWalnutObject,
-  };
+  } catch (err) {
+    if (err instanceof Error)
+      throw new Error(`Error in walnuts: ${err.message}`);
+    else throw err;
+  }
 }
