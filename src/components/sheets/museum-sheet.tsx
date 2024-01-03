694ea2cc713efa670265e7db645ebd5620b9ebd8
Jack LaFond <jack.lafond@icloud.com>
2024-01-02 22:18:59 -0700

    feat(museum): add mobile museum drawer

--- src/components/sheets/museum-sheet.tsx
+++ src/components/sheets/museum-sheet.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@react-hook/media-query";
 import Image from "next/image";
 
 import objects from "@/data/objects.json";
@@ -18,6 +19,14 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { CreatePlayerRedirect } from "../createPlayerRedirect";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "../ui/drawer";
+import { ScrollArea } from "../ui/scroll-area";
 
 interface Props {
   open: boolean;
@@ -26,6 +35,8 @@ interface Props {
 }
 
 export const MuseumSheet = ({ open, setIsOpen, trinket }: Props) => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
   const { activePlayer, patchPlayer } = useContext(PlayersContext);
 
   const [artifacts, minerals] = useMemo(() => {
@@ -82,102 +93,205 @@ export const MuseumSheet = ({ open, setIsOpen, trinket }: Props) => {
     setIsOpen(false);
   }
 
-  return (
-    <Sheet open={open} onOpenChange={setIsOpen}>
-      <SheetContent>
-        <SheetHeader className="mt-4">
-          <div className="flex justify-center">
-            <Image
-              src={iconURL}
-              alt={name ? name : "No Info"}
-              height={64}
-              width={64}
-            />
-          </div>
-          <SheetTitle className="text-center">
-            {name ? name : "No Info"}
-          </SheetTitle>
-          <SheetDescription className="text-center italic">
-            {description ? description : "No Description Found"}
-          </SheetDescription>
-        </SheetHeader>
-        {trinket && (
-          <div className="space-y-6 mt-4">
-            <section className="space-y-2">
-              <div className="grid grid-cols-1 gap-3">
-                {artifacts.has(parseInt(trinket.itemID)) ? (
-                  <Button
-                    variant="secondary"
-                    disabled={
-                      !activePlayer ||
-                      (!artifacts.has(parseInt(trinket.itemID)) &&
-                        !minerals.has(parseInt(trinket.itemID)))
-                    }
-                    data-umami-event="Set incompleted"
-                    onClick={() => {
-                      handleStatusChange(0);
-                    }}
-                  >
-                    Set Incomplete
-                  </Button>
-                ) : (
-                  <Button
-                    variant="secondary"
-                    disabled={
-                      !activePlayer ||
-                      artifacts.has(parseInt(trinket.itemID)) ||
-                      minerals.has(parseInt(trinket.itemID))
-                    }
-                    data-umami-event="Set completed"
-                    onClick={() => {
-                      handleStatusChange(2);
-                    }}
-                  >
-                    Set Completed
-                  </Button>
+  if (isDesktop) {
+    return (
+      <Sheet open={open} onOpenChange={setIsOpen}>
+        <SheetContent>
+          <SheetHeader className="mt-4">
+            <div className="flex justify-center">
+              <Image
+                src={iconURL}
+                alt={name ? name : "No Info"}
+                height={64}
+                width={64}
+              />
+            </div>
+            <SheetTitle className="text-center">
+              {name ? name : "No Info"}
+            </SheetTitle>
+            <SheetDescription className="text-center italic">
+              {description ? description : "No Description Found"}
+            </SheetDescription>
+          </SheetHeader>
+          {trinket && (
+            <div className="space-y-6 mt-4">
+              <section className="space-y-2">
+                <div className="grid grid-cols-1 gap-3">
+                  {artifacts.has(parseInt(trinket.itemID)) ? (
+                    <Button
+                      variant="secondary"
+                      disabled={
+                        !activePlayer ||
+                        (!artifacts.has(parseInt(trinket.itemID)) &&
+                          !minerals.has(parseInt(trinket.itemID)))
+                      }
+                      data-umami-event="Set incompleted"
+                      onClick={() => {
+                        handleStatusChange(0);
+                      }}
+                    >
+                      Set Incomplete
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="secondary"
+                      disabled={
+                        !activePlayer ||
+                        artifacts.has(parseInt(trinket.itemID)) ||
+                        minerals.has(parseInt(trinket.itemID))
+                      }
+                      data-umami-event="Set completed"
+                      onClick={() => {
+                        handleStatusChange(2);
+                      }}
+                    >
+                      Set Completed
+                    </Button>
+                  )}
+                  {!activePlayer && <CreatePlayerRedirect />}
+                </div>
+              </section>
+              <section className="space-y-2">
+                {trinket.locations && (
+                  <>
+                    <h3 className="font-semibold">Location</h3>
+                    <Separator />
+                    <ul className="list-disc list-inside">
+                      {trinket.locations.map((location) => (
+                        <li
+                          key={location}
+                          className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
+                        >
+                          {location}
+                        </li>
+                      ))}
+                    </ul>
+                  </>
                 )}
-                {!activePlayer && <CreatePlayerRedirect />}
-              </div>
-            </section>
-            <section className="space-y-2">
-              {trinket.locations && (
-                <>
-                  <h3 className="font-semibold">Location</h3>
-                  <Separator />
-                  <ul className="list-disc list-inside">
-                    {trinket.locations.map((location) => (
-                      <li
-                        key={location}
-                        className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
-                      >
-                        {location}
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
-            </section>
-            <section className="space-y-2">
-              {trinket.used_in && trinket.used_in.length > 0 && (
-                <>
-                  <h3 className="font-semibold">Used In</h3>
-                  <Separator />
-                  <ul className="list-disc list-inside">
-                    {trinket.used_in.map((location) => (
-                      <li
-                        key={location}
-                        className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
-                      >
-                        {location}
-                      </li>
-                    ))}
-                  </ul>
-                </>
-              )}
-            </section>
-          </div>
-        )}
-      </SheetContent>
-    </Sheet>
+              </section>
+              <section className="space-y-2">
+                {trinket.used_in && trinket.used_in.length > 0 && (
+                  <>
+                    <h3 className="font-semibold">Used In</h3>
+                    <Separator />
+                    <ul className="list-disc list-inside">
+                      {trinket.used_in.map((location) => (
+                        <li
+                          key={location}
+                          className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
+                        >
+                          {location}
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </section>
+            </div>
+          )}
+        </SheetContent>
+      </Sheet>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setIsOpen}>
+      <DrawerContent className="fixed bottom-0 left-0 right-0 max-h-[90dvh]">
+        <ScrollArea className="overflow-auto">
+          <DrawerHeader className="mt-4 -mb-4">
+            <div className="flex justify-center">
+              <Image
+                src={iconURL}
+                alt={name ? name : "No Info"}
+                height={64}
+                width={64}
+              />
+            </div>
+            <DrawerTitle className="text-center">
+              {name ? name : "No Info"}
+            </DrawerTitle>
+            <DrawerDescription className="text-center italic">
+              {description ? description : "No Description Found"}
+            </DrawerDescription>
+          </DrawerHeader>
+          {trinket && (
+            <div className="space-y-6 p-6">
+              <section className="space-y-2">
+                <div className="grid grid-cols-1 gap-3">
+                  {artifacts.has(parseInt(trinket.itemID)) ? (
+                    <Button
+                      variant="secondary"
+                      disabled={
+                        !activePlayer ||
+                        (!artifacts.has(parseInt(trinket.itemID)) &&
+                          !minerals.has(parseInt(trinket.itemID)))
+                      }
+                      data-umami-event="Set incompleted"
+                      onClick={() => {
+                        handleStatusChange(0);
+                      }}
+                    >
+                      Set Incomplete
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="secondary"
+                      disabled={
+                        !activePlayer ||
+                        artifacts.has(parseInt(trinket.itemID)) ||
+                        minerals.has(parseInt(trinket.itemID))
+                      }
+                      data-umami-event="Set completed"
+                      onClick={() => {
+                        handleStatusChange(2);
+                      }}
+                    >
+                      Set Completed
+                    </Button>
+                  )}
+                  {!activePlayer && <CreatePlayerRedirect />}
+                </div>
+              </section>
+              <section className="space-y-2">
+                {trinket.locations && (
+                  <>
+                    <h3 className="font-semibold">Location</h3>
+                    <Separator />
+                    <ul className="list-disc list-inside">
+                      {trinket.locations.map((location) => (
+                        <li
+                          key={location}
+                          className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
+                        >
+                          {location}
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </section>
+              <section className="space-y-2">
+                {trinket.used_in && trinket.used_in.length > 0 && (
+                  <>
+                    <h3 className="font-semibold">Used In</h3>
+                    <Separator />
+                    <ul className="list-disc list-inside">
+                      {trinket.used_in.map((location) => (
+                        <li
+                          key={location}
+                          className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm"
+                        >
+                          {location}
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
+              </section>
+            </div>
+          )}
+        </ScrollArea>
+      </DrawerContent>
+    </Drawer>
   );
 };
