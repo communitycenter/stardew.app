c27f51bddbc15e1a65a3c51116ba8780481ad563
Jack LaFond <jack.lafond@icloud.com>
2024-01-02 22:23:32 -0700

    feat(recipe): add mobile recipe sheet

--- src/components/sheets/recipe-sheet.tsx
+++ src/components/sheets/recipe-sheet.tsx
@@ -32,7 +32,16 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { useMediaQuery } from "@react-hook/media-query";
 import { CreatePlayerRedirect } from "../createPlayerRedirect";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "../ui/drawer";
+import { ScrollArea } from "../ui/scroll-area";
 
 interface Props<T extends Recipe> {
   open: boolean;
@@ -62,6 +71,7 @@ export const RecipeSheet = <T extends Recipe>({
 }: Props<T>) => {
   const { activePlayer, patchPlayer } = useContext(PlayersContext);
   const [status, setStatus] = useState(0);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   useEffect(() => {
     if (!activePlayer || !recipe) return;
@@ -133,129 +143,259 @@ export const RecipeSheet = <T extends Recipe>({
     }
   }
 
-  return (
-    <Sheet open={open} onOpenChange={setIsOpen}>
-      <SheetContent>
-        <SheetHeader className="mt-4">
-          <div className="flex justify-center">
-            <Image
-              src={iconURL}
-              alt={name ? name : "No Info"}
-              width={64}
-              height={
-                recipe && isCraftingRecipe(recipe)
-                  ? recipe.isBigCraftable
-                    ? 128
+  if (isDesktop) {
+    return (
+      <Sheet open={open} onOpenChange={setIsOpen}>
+        <SheetContent>
+          <SheetHeader className="mt-4">
+            <div className="flex justify-center">
+              <Image
+                src={iconURL}
+                alt={name ? name : "No Info"}
+                width={64}
+                height={
+                  recipe && isCraftingRecipe(recipe)
+                    ? recipe.isBigCraftable
+                      ? 128
+                      : 64
                     : 64
-                  : 64
-              }
-            />
-          </div>
-          <SheetTitle className="text-center">
-            {name ? name : "No Info"}
-          </SheetTitle>
-          <SheetDescription className="text-center italic">
-            {description ? description : "No Description Found"}
-          </SheetDescription>
-        </SheetHeader>
-        {recipe && (
-          <div className="space-y-6 mt-4">
-            <section className="space-y-2">
-              <h3 className="font-semibold">Actions</h3>
-              <Separator />
-              <Select
-                value={status.toString()}
-                onValueChange={(val) => handleStatusChange(parseInt(val))}
-                disabled={!activePlayer}
-              >
-                <SelectTrigger>
-                  <SelectValue placeholder="Change Status...">
-                    {status === 0
-                      ? "Unknown"
-                      : status === 1
-                      ? "Known"
-                      : "Completed"}
-                  </SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Change Status</SelectLabel>
-                    <SelectItem value="0">
-                      <div className="flex items-center gap-2">
-                        <div className="border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950 rounded-full h-4 w-4" />
-                        <p>Set Unknown</p>
-                      </div>
-                    </SelectItem>
-                    <SelectItem value="1">
-                      <div className="flex items-center gap-2">
-                        <div className="border border-yellow-900 bg-yellow-500/20 dark:bg-yellow-500/10 rounded-full h-4 w-4" />
-                        <p>Set Known</p>
-                      </div>
-                    </SelectItem>
-                    <SelectItem value="2">
-                      <div className="flex items-center gap-2">
-                        <div className="border border-green-900 bg-green-500/20 dark:bg-green-500/10 rounded-full h-4 w-4" />
-                        <p>Set Completed</p>
-                      </div>
-                    </SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <div>{!activePlayer && <CreatePlayerRedirect />}</div>
-            </section>
-            <section className="space-y-2">
-              <h3 className="font-semibold">How to unlock</h3>
-              <Separator />
-              <p className="text-sm text-neutral-500 dark:text-neutral-400">
-                {recipe.unlockConditions}
-              </p>
-            </section>
-            <section className="space-y-2">
-              <h3 className="font-semibold">Ingredients</h3>
-              <Separator />
-              <ul className="list-none list-inside space-y-3">
-                {recipe.ingredients.map((ingredient) => {
-                  let item;
+                }
+              />
+            </div>
+            <SheetTitle className="text-center">
+              {name ? name : "No Info"}
+            </SheetTitle>
+            <SheetDescription className="text-center italic">
+              {description ? description : "No Description Found"}
+            </SheetDescription>
+          </SheetHeader>
+          {recipe && (
+            <div className="space-y-6 mt-4">
+              <section className="space-y-2">
+                <h3 className="font-semibold">Actions</h3>
+                <Separator />
+                <Select
+                  value={status.toString()}
+                  onValueChange={(val) => handleStatusChange(parseInt(val))}
+                  disabled={!activePlayer}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Change Status...">
+                      {status === 0
+                        ? "Unknown"
+                        : status === 1
+                        ? "Known"
+                        : "Completed"}
+                    </SelectValue>
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Change Status</SelectLabel>
+                      <SelectItem value="0">
+                        <div className="flex items-center gap-2">
+                          <div className="border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950 rounded-full h-4 w-4" />
+                          <p>Set Unknown</p>
+                        </div>
+                      </SelectItem>
+                      <SelectItem value="1">
+                        <div className="flex items-center gap-2">
+                          <div className="border border-yellow-900 bg-yellow-500/20 dark:bg-yellow-500/10 rounded-full h-4 w-4" />
+                          <p>Set Known</p>
+                        </div>
+                      </SelectItem>
+                      <SelectItem value="2">
+                        <div className="flex items-center gap-2">
+                          <div className="border border-green-900 bg-green-500/20 dark:bg-green-500/10 rounded-full h-4 w-4" />
+                          <p>Set Completed</p>
+                        </div>
+                      </SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <div>{!activePlayer && <CreatePlayerRedirect />}</div>
+              </section>
+              <section className="space-y-2">
+                <h3 className="font-semibold">How to unlock</h3>
+                <Separator />
+                <p className="text-sm text-neutral-500 dark:text-neutral-400">
+                  {recipe.unlockConditions}
+                </p>
+              </section>
+              <section className="space-y-2">
+                <h3 className="font-semibold">Ingredients</h3>
+                <Separator />
+                <ul className="list-none list-inside space-y-3">
+                  {recipe.ingredients.map((ingredient) => {
+                    let item;
 
-                  // if itemID is greater than 0, it's an object
-                  if (ingredient.itemID > 0) {
-                    item =
-                      objects[
-                        ingredient.itemID.toString() as keyof typeof objects
-                      ];
-                  } else {
-                    // otherwise, it's a category
-                    item = {
-                      name: categoryItems[ingredient.itemID],
-                      iconURL: categoryIcons[ingredient.itemID],
-                    };
-                  }
+                    // if itemID is greater than 0, it's an object
+                    if (ingredient.itemID > 0) {
+                      item =
+                        objects[
+                          ingredient.itemID.toString() as keyof typeof objects
+                        ];
+                    } else {
+                      // otherwise, it's a category
+                      item = {
+                        name: categoryItems[ingredient.itemID],
+                        iconURL: categoryIcons[ingredient.itemID],
+                      };
+                    }
 
-                  return (
-                    <li
-                      key={ingredient.itemID}
-                      className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm font-semibold"
-                    >
-                      <div className="flex items-center space-x-2">
-                        <Image
-                          src={item.iconURL}
-                          alt={item.name}
-                          width={32}
-                          height={32}
-                          quality={25}
-                        />
-                        <p className="font-semibold">
-                          • {item.name} ({ingredient.quantity}x)
-                        </p>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            </section>
-          </div>
-        )}
-      </SheetContent>
-    </Sheet>
+                    return (
+                      <li
+                        key={ingredient.itemID}
+                        className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm font-semibold"
+                      >
+                        <div className="flex items-center space-x-2">
+                          <Image
+                            src={item.iconURL}
+                            alt={item.name}
+                            width={32}
+                            height={32}
+                            quality={25}
+                          />
+                          <p className="font-semibold">
+                            • {item.name} ({ingredient.quantity}x)
+                          </p>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </section>
+            </div>
+          )}
+        </SheetContent>
+      </Sheet>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setIsOpen}>
+      <DrawerContent className="fixed bottom-0 left-0 right-0 max-h-[90dvh]">
+        <ScrollArea className="overflow-auto">
+          <DrawerHeader className="mt-4 -mb-4">
+            <div className="flex justify-center">
+              <Image
+                src={iconURL}
+                alt={name ? name : "No Info"}
+                width={64}
+                height={
+                  recipe && isCraftingRecipe(recipe)
+                    ? recipe.isBigCraftable
+                      ? 128
+                      : 64
+                    : 64
+                }
+              />
+            </div>
+            <DrawerTitle className="text-center">
+              {name ? name : "No Info"}
+            </DrawerTitle>
+            <DrawerDescription className="text-center italic">
+              {description ? description : "No Description Found"}
+            </DrawerDescription>
+          </DrawerHeader>
+          {recipe && (
+            <div className="space-y-6 p-6">
+              <section className="space-y-2">
+                <h3 className="font-semibold">Actions</h3>
+                <Separator />
+                <Select
+                  value={status.toString()}
+                  onValueChange={(val) => handleStatusChange(parseInt(val))}
+                  disabled={!activePlayer}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Change Status...">
+                      {status === 0
+                        ? "Unknown"
+                        : status === 1
+                        ? "Known"
+                        : "Completed"}
+                    </SelectValue>
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Change Status</SelectLabel>
+                      <SelectItem value="0">
+                        <div className="flex items-center gap-2">
+                          <div className="border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950 rounded-full h-4 w-4" />
+                          <p>Set Unknown</p>
+                        </div>
+                      </SelectItem>
+                      <SelectItem value="1">
+                        <div className="flex items-center gap-2">
+                          <div className="border border-yellow-900 bg-yellow-500/20 dark:bg-yellow-500/10 rounded-full h-4 w-4" />
+                          <p>Set Known</p>
+                        </div>
+                      </SelectItem>
+                      <SelectItem value="2">
+                        <div className="flex items-center gap-2">
+                          <div className="border border-green-900 bg-green-500/20 dark:bg-green-500/10 rounded-full h-4 w-4" />
+                          <p>Set Completed</p>
+                        </div>
+                      </SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <div>{!activePlayer && <CreatePlayerRedirect />}</div>
+              </section>
+              <section className="space-y-2">
+                <h3 className="font-semibold">How to unlock</h3>
+                <Separator />
+                <p className="text-sm text-neutral-500 dark:text-neutral-400">
+                  {recipe.unlockConditions}
+                </p>
+              </section>
+              <section className="space-y-2">
+                <h3 className="font-semibold">Ingredients</h3>
+                <Separator />
+                <ul className="list-none list-inside space-y-3">
+                  {recipe.ingredients.map((ingredient) => {
+                    let item;
+
+                    // if itemID is greater than 0, it's an object
+                    if (ingredient.itemID > 0) {
+                      item =
+                        objects[
+                          ingredient.itemID.toString() as keyof typeof objects
+                        ];
+                    } else {
+                      // otherwise, it's a category
+                      item = {
+                        name: categoryItems[ingredient.itemID],
+                        iconURL: categoryIcons[ingredient.itemID],
+                      };
+                    }
+
+                    return (
+                      <li
+                        key={ingredient.itemID}
+                        className="mt-1 text-neutral-500 dark:text-neutral-400 text-sm font-semibold"
+                      >
+                        <div className="flex items-center space-x-2">
+                          <Image
+                            src={item.iconURL}
+                            alt={item.name}
+                            width={32}
+                            height={32}
+                            quality={25}
+                          />
+                          <p className="font-semibold">
+                            • {item.name} ({ingredient.quantity}x)
+                          </p>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </section>
+            </div>
+          )}
+        </ScrollArea>
+      </DrawerContent>
+    </Drawer>
   );
 };
