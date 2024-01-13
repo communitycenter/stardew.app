bdc78d880375e8126ce4e50c231d095e3108dfe6
Jack LaFond <jack.lafond@icloud.com>
2024-01-13 13:31:13 -0500

    dev: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -27,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
