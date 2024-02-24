d35e47d2ffbcecf46f236188641eaa4beed4610f
Jack LaFond <jack.lafond@icloud.com>
2024-02-24 18:42:01 -0500

    feat(cooking) hide 1.6 based on cookie etc

--- src/pages/crafting.tsx
+++ src/pages/crafting.tsx
@@ -21,6 +21,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Command, CommandInput } from "@/components/ui/command";
+import { getCookie } from "cookies-next";
 
 const semverGte = require("semver/functions/gte");
 
@@ -45,6 +46,7 @@ export default function Crafting() {
   const { activePlayer } = useContext(PlayersContext);
 
   useEffect(() => {
+    const wantsToSeeUpdate = getCookie("disable_1_6");
     if (activePlayer) {
       if (activePlayer.crafting?.recipes) {
         setPlayerRecipes(activePlayer.crafting.recipes);
@@ -54,13 +56,25 @@ export default function Crafting() {
       if (activePlayer.general?.gameVersion) {
         const version = activePlayer.general.gameVersion;
         setGameVersion(version);
+      } else {
+        if (!wantsToSeeUpdate) {
+          setGameVersion("1.6.0");
+        } else {
+          setGameVersion("1.5.4");
+        }
+      }
 
-        reqs["Craft Master"] = Object.values(recipes).filter((r) =>
-          semverGte(version, r.minVersion)
-        ).length;
+      reqs["Craft Master"] = Object.values(recipes).filter((r) =>
+        semverGte(gameVersion, r.minVersion)
+      ).length;
+    } else {
+      if (!wantsToSeeUpdate) {
+        setGameVersion("1.6.0");
+      } else {
+        setGameVersion("1.5.4");
       }
     }
-  }, [activePlayer]);
+  }, [activePlayer, gameVersion]);
 
   // calculate craftedCount here (all values of 2)
   const craftedCount = useMemo(() => {
