f004719e31f7373b1b8e3a0cf5a17798c8874225
Jack LaFond <jack.lafond@icloud.com>
2024-02-26 13:55:41 -0500

    update design of new item badge

--- src/components/new-item-badge.tsx
+++ src/components/new-item-badge.tsx
@@ -1,6 +1,6 @@
 export function NewItemBadge({ children }: { children: React.ReactNode }) {
   return (
-    <span className="absolute -right-3 -top-3 rounded-full bg-neutral-700 px-2 py-1 text-xs text-white dark:bg-neutral-800">
+    <span className="absolute -right-3 -top-3 rounded-full bg-white border border-neutral-200 dark:border-neutral-600 text-black  px-2 py-1 text-xs dark:text-white dark:bg-neutral-800">
       {children}
     </span>
   );
