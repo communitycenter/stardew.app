0f85491b4378103dcc83b4cd6094735a846b373b
Jack LaFond <jack.lafond@icloud.com>
2024-03-02 14:03:08 -0500

    fix posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,6 +22,9 @@ if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
     ui_host: "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(false); // debug mode in development
+    },
   });
 }
 
