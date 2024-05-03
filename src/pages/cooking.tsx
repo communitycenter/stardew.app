916cf2f12b6ee757b5881cb6d068fc4562beb89a
esummins <esummins@gmail.com>
2024-05-03 15:55:45 -0700

    Moved cooking page over to new format

--- src/pages/cooking.tsx
+++ src/pages/cooking.tsx
@@ -22,6 +22,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Command, CommandInput } from "@/components/ui/command";
+import PageWrapper from "@/components/page-wrapper";
 
 const semverGte = require("semver/functions/gte");
 
@@ -98,152 +99,137 @@ export default function Cooking() {
   };
 
   return (
-    <>
-      <Head>
-        <title>stardew.app | Cooking</title>
-        <meta
-          name="title"
-          content="Stardew Valley Cooking Recipes | stardew.app"
-        />
-        <meta
-          name="description"
-          content="Find and discover new cooking recipes in the Stardew Valley 1.6 update. Keep track of the recipes you've learned and monitor your progress towards becoming a skilled chef."
-        />
-        <meta
-          name="og:description"
-          content="Find and discover new cooking recipes in the Stardew Valley 1.6 update. Keep track of the recipes you've learned and monitor your progress towards becoming a skilled chef."
-        />
-        <meta
-          name="twitter:description"
-          content="Find and discover new cooking recipes in the Stardew Valley 1.6 update. Keep track of the recipes you've learned and monitor your progress towards becoming a skilled chef."
-        />
-        <meta
-          name="keywords"
-          content="stardew valley cooking recipe tracker, stardew valley cooking recipes, stardew valley recipe collection, stardew valley skilled chef, stardew valley recipe progress, stardew valley culinary skills, stardew valley gameplay tracker, stardew valley, stardew, cooking tracker, stardew valley, stardew, stardew checkup, stardew bundles, stardew 100% completion, stardew perfection tracker, stardew, valley"
-        />
-      </Head>
-      <main
-        className={`flex min-h-screen border-neutral-200 px-5 pb-8 pt-2 dark:border-neutral-800 md:border-l md:px-8`}
-      >
-        <div className="mx-auto mt-4 w-full space-y-4">
-          <h1 className="ml-1 text-2xl font-semibold text-gray-900 dark:text-white">
-            Cooking Tracker
-          </h1>
-          {/* Achievements Section */}
-          <Accordion type="single" collapsible defaultValue="item-1" asChild>
-            <section className="space-y-3">
-              <AccordionItem value="item-1">
-                <AccordionTrigger className="ml-1 pt-0 text-xl font-semibold text-gray-900 dark:text-white">
-                  Achievements
-                </AccordionTrigger>
-                <AccordionContent asChild>
-                  <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-                    {Object.values(achievements)
-                      .filter((a) => a.description.includes("Cook"))
-                      .map((achievement) => {
-                        const { completed, additionalDescription } =
-                          getAchievementProgress(achievement.name);
-
-                        return (
-                          <AchievementCard
-                            key={achievement.id}
-                            achievement={achievement}
-                            completed={completed}
-                            additionalDescription={additionalDescription}
-                          />
-                        );
-                      })}
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            </section>
-          </Accordion>
-          {/* All Recipes Section */}
+    <PageWrapper
+      title="Stardew Valley Cooking Recipes"
+      description="Find and discover new cooking recipes in the Stardew Valley 1.6 update. Keep track of the recipes you've learned and monitor your progress towards becoming a skilled chef."
+      pageKeywords={[
+        "stardew valley cooking recipe tracker",
+        "stardew valley cooking recipes",
+        "stardew valley recipe collection",
+        "stardew valley skilled chef",
+        "stardew valley recipe progress",
+        "stardew valley culinary skills",
+        "cooking tracker",
+      ]}
+    >
+      <div className="mx-auto mt-4 w-full space-y-4">
+        <h1 className="ml-1 text-2xl font-semibold text-gray-900 dark:text-white">
+          Cooking Tracker
+        </h1>
+        {/* Achievements Section */}
+        <Accordion type="single" collapsible defaultValue="item-1" asChild>
           <section className="space-y-3">
-            <h3 className="ml-1 text-xl font-semibold text-gray-900 dark:text-white">
-              All Recipes
-            </h3>
-            {/* Filters */}
-            <div className="grid grid-cols-1 justify-between gap-2 lg:flex">
-              <div className="grid grid-cols-2 gap-2 sm:flex sm:gap-3">
-                <FilterButton
-                  target={"0"}
-                  _filter={_filter}
-                  title={`Unknown (${
-                    reqs["Gourmet Chef"] - (knownCount + cookedCount)
-                  })`}
-                  setFilter={setFilter}
-                />
-                <FilterButton
-                  target={"1"}
-                  _filter={_filter}
-                  title={`Known (${knownCount})`}
-                  setFilter={setFilter}
-                />
-                <FilterButton
-                  target={"2"}
-                  _filter={_filter}
-                  title={`Cooked (${cookedCount})`}
-                  setFilter={setFilter}
-                />
-              </div>
-              <Command className="max-w-xs border border-b-0 dark:border-neutral-800">
-                <CommandInput
-                  onValueChange={(v) => setSearch(v)}
-                  placeholder="Search Recipes"
-                />
-              </Command>
-            </div>
-            {/* Cards */}
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
-              {Object.values(recipes)
-                .filter((r) => semverGte(gameVersion, r.minVersion))
-                .filter((r) => {
-                  if (!search) return true;
-                  const name = objects[r.itemID as keyof typeof objects].name;
-                  return name.toLowerCase().includes(search.toLowerCase());
-                })
-                .filter((r) => {
-                  if (_filter === "0") {
-                    // unknown recipes (not in playerRecipes)
-                    return !(
-                      r.itemID in playerRecipes && playerRecipes[r.itemID] > 0
-                    );
-                  } else if (_filter === "1") {
-                    // known recipes (in playerRecipes) and not cooked
-                    return (
-                      r.itemID in playerRecipes && playerRecipes[r.itemID] === 1
-                    );
-                  } else if (_filter === "2") {
-                    // cooked recipes (in playerRecipes) and cooked
-                    return (
-                      r.itemID in playerRecipes && playerRecipes[r.itemID] === 2
-                    );
-                  } else return true; // all recipes
-                })
-                .map((f) => (
-                  <RecipeCard
-                    key={f.itemID}
-                    recipe={f}
-                    status={
-                      f.itemID in playerRecipes ? playerRecipes[f.itemID] : 0
-                    }
-                    setIsOpen={setIsOpen}
-                    setObject={setRecipe}
-                    setPromptOpen={setPromptOpen}
-                    show={show}
-                  />
-                ))}
-            </div>
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="ml-1 pt-0 text-xl font-semibold text-gray-900 dark:text-white">
+                Achievements
+              </AccordionTrigger>
+              <AccordionContent asChild>
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+                  {Object.values(achievements)
+                    .filter((a) => a.description.includes("Cook"))
+                    .map((achievement) => {
+                      const { completed, additionalDescription } =
+                        getAchievementProgress(achievement.name);
+
+                      return (
+                        <AchievementCard
+                          key={achievement.id}
+                          achievement={achievement}
+                          completed={completed}
+                          additionalDescription={additionalDescription}
+                        />
+                      );
+                    })}
+                </div>
+              </AccordionContent>
+            </AccordionItem>
           </section>
-        </div>
-        <RecipeSheet open={open} setIsOpen={setIsOpen} recipe={recipe} />
-        <UnblurDialog
-          open={showPrompt}
-          setOpen={setPromptOpen}
-          toggleShow={toggleShow}
-        />
-      </main>
-    </>
+        </Accordion>
+        {/* All Recipes Section */}
+        <section className="space-y-3">
+          <h3 className="ml-1 text-xl font-semibold text-gray-900 dark:text-white">
+            All Recipes
+          </h3>
+          {/* Filters */}
+          <div className="grid grid-cols-1 justify-between gap-2 lg:flex">
+            <div className="grid grid-cols-2 gap-2 sm:flex sm:gap-3">
+              <FilterButton
+                target={"0"}
+                _filter={_filter}
+                title={`Unknown (${
+                  reqs["Gourmet Chef"] - (knownCount + cookedCount)
+                })`}
+                setFilter={setFilter}
+              />
+              <FilterButton
+                target={"1"}
+                _filter={_filter}
+                title={`Known (${knownCount})`}
+                setFilter={setFilter}
+              />
+              <FilterButton
+                target={"2"}
+                _filter={_filter}
+                title={`Cooked (${cookedCount})`}
+                setFilter={setFilter}
+              />
+            </div>
+            <Command className="max-w-xs border border-b-0 dark:border-neutral-800">
+              <CommandInput
+                onValueChange={(v) => setSearch(v)}
+                placeholder="Search Recipes"
+              />
+            </Command>
+          </div>
+          {/* Cards */}
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
+            {Object.values(recipes)
+              .filter((r) => semverGte(gameVersion, r.minVersion))
+              .filter((r) => {
+                if (!search) return true;
+                const name = objects[r.itemID as keyof typeof objects].name;
+                return name.toLowerCase().includes(search.toLowerCase());
+              })
+              .filter((r) => {
+                if (_filter === "0") {
+                  // unknown recipes (not in playerRecipes)
+                  return !(
+                    r.itemID in playerRecipes && playerRecipes[r.itemID] > 0
+                  );
+                } else if (_filter === "1") {
+                  // known recipes (in playerRecipes) and not cooked
+                  return (
+                    r.itemID in playerRecipes && playerRecipes[r.itemID] === 1
+                  );
+                } else if (_filter === "2") {
+                  // cooked recipes (in playerRecipes) and cooked
+                  return (
+                    r.itemID in playerRecipes && playerRecipes[r.itemID] === 2
+                  );
+                } else return true; // all recipes
+              })
+              .map((f) => (
+                <RecipeCard
+                  key={f.itemID}
+                  recipe={f}
+                  status={
+                    f.itemID in playerRecipes ? playerRecipes[f.itemID] : 0
+                  }
+                  setIsOpen={setIsOpen}
+                  setObject={setRecipe}
+                  setPromptOpen={setPromptOpen}
+                  show={show}
+                />
+              ))}
+          </div>
+        </section>
+      </div>
+      <RecipeSheet open={open} setIsOpen={setIsOpen} recipe={recipe} />
+      <UnblurDialog
+        open={showPrompt}
+        setOpen={setPromptOpen}
+        toggleShow={toggleShow}
+      />
+    </PageWrapper>
   );
 }
