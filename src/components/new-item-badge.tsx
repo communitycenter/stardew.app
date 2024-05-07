ddc795b5ae2f4a184bd85e44ba795b97a7322fc9
esummins <esummins@gmail.com>
2024-05-06 23:19:13 -0700

    More chiclet updates

--- src/components/new-item-badge.tsx
+++ src/components/new-item-badge.tsx
@@ -15,7 +15,7 @@ export function NewItemBadge({
 }) {
   return (
     <span
-      className={`absolute ${position === "inside" ? "right-4 top-4 rounded-full" : "-right-px -top-px rounded-bl-lg rounded-tr-lg"} z-0 border border-neutral-200 bg-white px-2 py-1  text-xs text-black dark:border-neutral-600 dark:bg-neutral-800 dark:text-white`}
+      className={`absolute ${position === "inside" ? "right-4 top-4 rounded-full border" : "right-0 top-0 rounded-bl-lg rounded-tr-lg border-b border-l"} z-0 border-neutral-200 bg-white px-2 py-1  text-xs text-black dark:border-neutral-600 dark:bg-neutral-800 dark:text-white`}
     >
       ✨ {removeTrailingZeroVersions(version)}
     </span>
