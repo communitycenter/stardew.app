1ba537952e5c63ddc747b86c24de65654baf0686
Ian Mitchell <ian.mitchell@hey.com>
2025-07-07 19:16:24 -0700

    Formatting

--- apps/stardew.app/src/pages/_error.tsx
+++ apps/stardew.app/src/pages/_error.tsx
@@ -1,11 +1,13 @@
 import Error from "next/error";
 
+// @ts-expect-error Pages router
 const CustomErrorComponent = (props) => {
-  return <Error statusCode={props.statusCode} />;
+	return <Error statusCode={props.statusCode} />;
 };
 
+// @ts-expect-error Pages router
 CustomErrorComponent.getInitialProps = async (contextData) => {
-  return Error.getInitialProps(contextData);
+	return Error.getInitialProps(contextData);
 };
 
 export default CustomErrorComponent;
